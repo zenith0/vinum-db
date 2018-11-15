a0efcc30a2f59b1089ceb06357b0d62e76bbc600
Stefan Perndl <stefan.perndl@sophos.com>
2018-11-15 09:37:39 +0100

    added new production database settings

--- vinum/wsgi.py
+++ vinum/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vinum.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vinum.prodsettings')
 
 application = get_wsgi_application()
